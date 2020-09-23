328e4491785f3f8d409dd33dc5956df4a0cac197
S. M. JAHANGIR <smjxpro@gmail.com>
2020-09-23 23:54:45 +0600

    written if elif else conditional statement

---
+++ conditional-statements.py
@@ -0,0 +1,9 @@
+x = int(input("Enter the value of x: "))
+y = int(input("Enter the value of y: "))
+
+if x == y:
+	print("x and y are equal")
+elif x > y:
+	print("x is greater than y")
+else:
+	print("y is greater than x")
